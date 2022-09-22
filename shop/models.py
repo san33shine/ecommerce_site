d1d8524e2b17229146515741a08cec89c3ea0b31
San Chua <san.chua@iotamedtech.com>
2022-09-22 23:27:14 +0800

    Added pricing and fix cart popover pagination problem

--- shop/models.py
+++ shop/models.py
@@ -8,3 +8,13 @@ class Product(models.Model):
     category = models.CharField(max_length=200)
     description = models.TextField()
     image = models.CharField(max_length=300)
+
+class Order(models.Model):
+    items = models.CharField(max_length=1000)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=1000)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zipcode = models.CharField(max_length=100)
+    total = models.CharField(max_length=200)
